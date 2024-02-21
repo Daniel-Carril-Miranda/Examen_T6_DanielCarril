70ed28c5453ca15996ad4debc3f3e63cbf0fbee6
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2024-02-21 10:39:07 +0100

    Acabo js

--- build/js/app.js
+++ build/js/app.js
@@ -27,7 +27,7 @@ function crearGaleria() {
         imagen.innerHTML = `
         <source srcset="build/img/thumb/${i}.png" type="image/jpeg">
         <source srcset="build/img/thumb/${i}.jpg" type="image/webp">
-        <img loading="lazy" width="200" height="200" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
         `;
         imagen.onclick = function() {
             mostrarImagen(i);
@@ -43,7 +43,7 @@ function mostrarImagen(id) {
     <source srcset="build/img/grande/${id}.png" type="image/jpeg">
     <source srcset="build/img/grande/${id}.jpg" type="image/jpeg">
     <source srcset="build/img/grande/${id}.webp" type="image/webp">
-    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+    <img loading="lazy" width="200" height="200" src="build/img/grande/${id}.jpg" alt="imagen galeria">
     `;
  
     // Crea el Overlay con la imagen
@@ -72,4 +72,64 @@ function mostrarImagen(id) {
     body.appendChild(overlay);
     body.classList.add('fijar-body');
 }
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth"});
+        });
+    });
+}
+
+/** Función para implementar una barra de navegación fija en una página web, que se congela cuando el/la usuario/a hace scroll hacia abajo y alcanza una sección específica del documento **/
+ 
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const video = document.querySelector('.video');
+    const body = document.querySelector('body');
+ 
+ 
+    window.addEventListener('scroll', function() {
+        // Si el usuario ha  hecho scroll hacia abajo lo suficiente como para
+        // llegar a la sección "galeria"
+        if( video.getBoundingClientRect().bottom < 0 && this.window.innerWidth >= 768 ) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+ 
+/** Función para mejorar la experiencia de desplazamiento en una página web al hacer clic en los enlaces de navegación **/
+ 
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+ 
+    // Un evento click para cada enlace
+ 
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            // Le quitamos el comportamiento predeterminado al enlace
+            e.preventDefault();
+ 
+            // Se obtiene el valor del atributo href del enlace en el que se hizo clic. 
+            const seccionScroll = e.target.attributes.href.value;
+            // Selección de la sección de destino
+            const seccion = document.querySelector(seccionScroll);
+            // Desplazamiento suave hacia la sección
+            seccion.scrollIntoView({ behavior: "smooth"});
+        });
+    });
+}
+
+ 
+
+ 
+
 
